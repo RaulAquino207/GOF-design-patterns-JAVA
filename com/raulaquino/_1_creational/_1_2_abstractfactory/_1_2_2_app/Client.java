942b0b77f100a496903c1a7f9944c894d22a9c81
Raul Aquino <aquinoraul207@gmail.com>
2024-11-01 23:20:16 -0300

    [main] add of two examples about abstractfactory

--- com/raulaquino/_1_creational/_1_2_abstractfactory/_1_2_2_app/Client.java
+++
@@ -1,4 +0,0 @@
-package com.raulaquino._1_creational._1_2_abstractfactory._1_2_2_app;
-
-public class Client {
-}
---
+++ com/raulaquino/_1_creational/_1_2_abstractfactory/_1_2_2_apps/_1_2_2_1_bymodelfactory/Client.java
@@ -0,0 +1,4 @@
+package com.raulaquino._1_creational._1_2_abstractfactory._1_2_2_apps._1_2_2_1_bymodelfactory;
+
+public class Client {
+}
